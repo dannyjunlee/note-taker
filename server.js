6941f5e9e46bbd9b479dc91bcb2881ac4630b109
Danny <dl667@cornell.edu>
2022-10-28 19:18:46 -0400

    worked on frontend functionality through localhost

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
 // Wildcard function
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
